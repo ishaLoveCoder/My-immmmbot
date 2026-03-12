4cbd164efbf1691ce1833a2bafbec47b730ee893
ishaLoveCoder <isha1470@outlook.com>
2026-03-12 13:07:28 +0530

    Enhance movie details extraction with actors and directors
    Added functionality to extract actors and directors from movie data.

--- imdb.py
+++ imdb.py
@@ -2,6 +2,7 @@ import aiohttp
 from bs4 import BeautifulSoup
 import json
 
+
 async def search_movie(query):
 
     url = f"https://m.imdb.com/find?q={query}&s=tt"
@@ -10,9 +11,9 @@ async def search_movie(query):
         async with session.get(url) as resp:
             html = await resp.text()
 
-    soup = BeautifulSoup(html,"html.parser")
+    soup = BeautifulSoup(html, "html.parser")
 
-    script = soup.find("script",{"id":"__NEXT_DATA__"})
+    script = soup.find("script", {"id": "__NEXT_DATA__"})
     data = json.loads(script.text)
 
     results = data["props"]["pageProps"]["titleResults"]["results"]
@@ -26,7 +27,7 @@ async def search_movie(query):
         movies.append({
             "id": item["titleId"],
             "title": item["titleText"],
-            "year": item.get("releaseYear","")
+            "year": item.get("releaseYear", "")
         })
 
     return movies
@@ -40,24 +41,59 @@ async def get_movie(imdb_id):
         async with session.get(url) as resp:
             html = await resp.text()
 
-    soup = BeautifulSoup(html,"html.parser")
+    soup = BeautifulSoup(html, "html.parser")
 
-    script = soup.find("script",{"id":"__NEXT_DATA__"})
+    script = soup.find("script", {"id": "__NEXT_DATA__"})
     data = json.loads(script.text)
 
     movie = data["props"]["pageProps"]["aboveTheFoldData"]
 
+    # actors
+    actors = []
+
+    try:
+        for c in movie["castV2"][0]["credits"][:10]:
+            actors.append(c["name"]["nameText"]["text"])
+    except:
+        pass
+
+    # directors
+    directors = []
+
+    try:
+        for c in movie["principalCredits"]:
+            if c["category"]["text"] == "Director":
+                for d in c["credits"]:
+                    directors.append(d["name"]["nameText"]["text"])
+    except:
+        pass
+
     return {
 
         "TITLE": movie["titleText"]["text"],
+
         "YEAR": movie["releaseYear"]["year"],
+
         "RATING": movie["ratingsSummary"]["aggregateRating"],
+
         "VOTES": movie["ratingsSummary"]["voteCount"],
+
         "DURATION": movie["runtime"]["displayableProperty"]["value"]["plainText"],
+
         "GENRE": ", ".join([g["text"] for g in movie["genres"]["genres"]]),
+
         "STORY_LINE": movie["plot"]["plotText"]["plainText"],
+
         "LANGUAGE": "English",
+
         "IMDB_ID": imdb_id,
+
         "IMDB_URL": f"https://imdb.com/title/{imdb_id}",
-        "IMG_POSTER": soup.find("meta",property="og:image")["content"]
+
+        "ACTORS": ", ".join(actors),
+
+        "DIRECTORS": ", ".join(directors),
+
+        "IMG_POSTER": soup.find("meta", property="og:image")["content"]
+
     }
